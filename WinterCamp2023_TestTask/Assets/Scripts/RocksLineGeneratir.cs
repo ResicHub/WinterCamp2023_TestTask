c7912497b240938a9bf4dfd380ceea86909903ea
ResicHub <resic.resic@yandex.ru>
2023-01-04 12:11:12 +0700

    Making of prototype scene 1
    Assembled a small game scene, arranged the details. Now I'm working on generating a line of rocks as a playing field.

---
+++ WinterCamp2023_TestTask/Assets/Scripts/RocksLineGeneratir.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RocksLineGeneratir : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+}
