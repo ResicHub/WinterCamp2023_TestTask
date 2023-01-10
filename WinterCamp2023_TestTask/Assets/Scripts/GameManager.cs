336356d7a483e34ed99229ff8272827250ae875c
ResicHub <resic.resic@yandex.ru>
2023-01-10 15:32:11 +0700

    Working with design
    Change island mesh, add water and setting a sunlight for better view of game.

---
+++ WinterCamp2023_TestTask/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform mainCamera;
+
+    private float mainCameraSpeed = 50f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            mainCamera.RotateAround(Vector3.zero, Vector3.up, - mainCameraSpeed * Time.fixedDeltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            mainCamera.RotateAround(Vector3.zero, Vector3.up, mainCameraSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
